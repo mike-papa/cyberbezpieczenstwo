146437bb2c1c9b5c1403f86e3c3a15e36f1bb5ef
mike-papa <73196026+mike-papa@users.noreply.github.com>
2024-05-05 19:01:38 +0200

    Dodaj zadanie 3 cz.2 (popraw podatnosci)

--- Zadanie 3/main.py
+++ Zadanie 3/main.py
@@ -1,15 +1,17 @@
 import mysql.connector
 import os
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="150.254.36.243",
-        user="p4_12345",
-        password="12345",
-        database="cb12345"
+        host="127.0.0.1",
+        port=3307,
+        user="root",
+        password="1234",
+        database="cyberpezpieczenstwo"
     )
     return connection
 
@@ -19,7 +21,8 @@ def login():
     password = request.form['password']
     with get_db_connection() as connection:
         cursor = connection.cursor(dictionary=True)
-        cursor.execute(f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'")
+        query = "SELECT * FROM users WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, password))
         user = cursor.fetchone()
     if user:
         return f"Welcome, {user['username']}!"
@@ -29,12 +32,18 @@ def login():
 @app.route('/display_message')
 def display_message():
     message = request.args.get('message')
-    return render_template_string(open('templates/message.html').read().replace('{{message}}', message))
+    return render_template('message.html', message=message)
 
 @app.route('/download_template')
 def download_template():
     file = request.args.get('file')
-    return render_template_string(open(os.path.join('templates', file)).read())
+    secure_path = secure_filename(file)
+    full_path = os.path.join('templates', secure_path)
+
+    if not os.path.exists(full_path) or not full_path.endswith('.html'):
+        return "Invalid file requested"
+
+    return render_template_string(open(full_path).read())
 
 if __name__ == '__main__':
     app.run()
